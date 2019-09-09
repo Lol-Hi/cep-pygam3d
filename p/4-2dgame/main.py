685e9a1fda6396c9b71b157f75234c4d74e6c578
Koh Luck Heng <luckheng.koh@gmail.com>
2019-09-09 12:45:33 +0800

    integrated mapmaker

--- p/4-2dgame/main.py
+++ p/4-2dgame/main.py
@@ -14,19 +14,39 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         pg.key.set_repeat(500, 100)
+        self.level = "sample" #for testing purposes, will change later
 
     def new(self):
         self.all_sprites = pg.sprite.Group()
         self.civilians = pg.sprite.Group()
         self.obstacles = pg.sprite.Group()
-        #testing only
-        self.player = Player(self, 4, 5)
-        Obstacle(self, 5, 5)
-        Terrorist(self, 10, 5)
-        Terrorist(self, 12, 7)
-        Civilian(self, 5, 12)
-        Civilian(self, 9, 2)
-        Civilian(self, 13, 11)
+        # #testing only
+        # self.player = Player(self, 4, HUMAN_HEIGHT, 5)
+        # Obstacle(self, 5, WALL_HEIGHT, 5)
+        # Terrorist(self, 10, HUMAN_HEIGHT, 5)
+        # Terrorist(self, 12, HUMAN_HEIGHT, 7)
+        # Civilian(self, 5, HUMAN_HEIGHT, 12)
+        # Civilian(self, 9, HUMAN_HEIGHT, 2)
+        # Civilian(self, 13, HUMAN_HEIGHT, 11)
+        self.mapreader()
+
+
+    def mapreader(self):
+        with open("./maps/{}.txt".format(self.level)) as mapfile:
+            map_width, map_height = mapfile.readline().split(",")
+            game_map = mapfile.readline().split(",")
+            for row in range(0, int(map_width)):
+                for col in range(0, int(map_height)):
+                    index = row*int(map_width)+col
+                    tile = game_map[index]
+                    if tile == "1":
+                        self.player = Player(self, row, HUMAN_HEIGHT, col)
+                    if tile == "2":
+                        Civilian(self, row, HUMAN_HEIGHT, col)
+                    if tile == "3":
+                        Terrorist(self, row, HUMAN_HEIGHT, col)
+                    if tile == "4":
+                        Obstacle(self, row, WALL_HEIGHT, col)
 
     def run(self):
         self.playing = True
--- p/4-2dgame/settings.py
+++ p/4-2dgame/settings.py
@@ -11,16 +11,18 @@ BLUE = (0, 0, 255)
 LIGHTBLUE = (173, 216, 230)
 YELLOW = (255, 255, 0)
 BEIGE = (245, 245, 220)
+PURPLE = (128, 0, 128)
 
 
 # Game settings
-WIDTH = 640
-HEIGHT = 640
-TILESIZE = 40
+WIDTH = 750
+HEIGHT = 750
+TILESIZE = 15
 FPS = 60
 TITLE = "Run, Hide, Tell!"
 
 # Human settings
+HUMAN_HEIGHT = 5
 SIGHT_RANGE = math.pi/3
 HUMAN_TURN = math.pi/300
 
@@ -34,3 +36,8 @@ SHOOT_INTERVAL = 5
 BULLET_WIDTH = 2
 BULLET_HEIGHT = 2
 BULLET_SPEED = 500
+
+# Obstacle settings
+
+# Wall settings
+WALL_HEIGHT = 8
